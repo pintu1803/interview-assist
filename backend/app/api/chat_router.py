f886fc5dcbca9e9bbc30025886ce61ae5a0b1e53
Pintu Saini <pintusaininch2979@gmail.com>
2026-09-26 16:25:41 +0530

    api works

---
+++ backend/app/api/chat_router.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter
+from app.api.schemas.chat import (AskRequest, AskResponse)
+from app.factory.rag_factory import create_rag_service
+
+#load only once at startup
+rag_service = create_rag_service()
+
+router = APIRouter(prefix="/chat", tags=["Chat"])
+
+@router.post("/ask", response_model=AskResponse)
+def ask_question(request : AskRequest):
+
+    #extract query from request and pass to inference engine
+    answer = rag_service.ask(request.question)
+
+    #wrap the response in AskResponse format and return
+    return AskResponse(answer=answer)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 
 from fastapi import FastAPI
 from app.api.health import router as health_router
+from app.api.chat_router import router as chat_router
 
-from backend.ingest import main
+# from backend.ingest import main
 
 app = FastAPI(
     title="GenAI Interview Prep Platform"
 )
 
 app.include_router(health_router)
+app.include_router(chat_router)
